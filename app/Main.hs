bcaf01af9b96f31272e735c34f4d18bd4f8dd0b6
Chin-Yun Yu <chin-yun.yu@qmul.ac.uk>
2024-01-09 18:36:47 +0800

    first workable version

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,87 @@
 module Main (main) where
 
-import Lib
+import Control.Concurrent
+import Control.Monad (forever)
+import qualified Data.Map as Map
+import LookUp (availableMessageContent, availableUserName)
+import System.Environment
+import System.Random
+import System.Random.Shuffle
+import Types
+
+receiverProcess :: Chan MessagePack -> ChatBoxes -> IO ()
+receiverProcess incomingMessages chatBoxes = do
+  msg <- readChan incomingMessages
+  let username = fromUser msg
+      msgContent = content msg
+      chatBox = chatBoxes Map.! username
+  addMessages chatBox msgContent
+  return ()
+
+userProcess :: User -> Chan MessagePack -> IO ()
+userProcess user sendChan = forever $ do
+  msgs <- mapM readMessages (Map.elems $ chatBoxes user) >>= return . concat
+  -- putStrLn $ "User " ++ name user ++ " received " ++ show (length msgs) ++ " messages."
+
+  -- wait for a random time and send a message to a random user
+  gen <- newStdGen
+  let (randomDelay, gen') = randomR (1, 1000000) gen :: (Int, StdGen)
+      (randomUserIdx, gen'') = randomR (0, Map.size (chatBoxes user) - 1) gen' :: (Int, StdGen)
+      (randomMsgIdx, _) = randomR (0, length availableMessageContent - 1) gen'' :: (Int, StdGen)
+      randomUser = Map.keys (chatBoxes user) !! randomUserIdx
+      randomMsg = availableMessageContent !! randomMsgIdx
+
+  -- putStrLn $ name user ++ show randomDelay ++ " " ++ randomUser ++ " " ++ randomMsg
+
+  threadDelay randomDelay
+
+  writeChan sendChan MessagePack {content = randomMsg, fromUser = name user, toUser = randomUser}
+
+-- increment the counter
+-- modifyMVar_ msgCounter (\x -> return $ x + 1)
+-- userProcess user msgCounter
+
+run :: Int -> Int -> Chan MessagePack -> Map.Map UserName (Chan MessagePack) -> IO ()
+run _ 0 _ _ = return ()
+run c maxNumMsg mainChan userChans = do
+  putStrLn $ "Round " ++ show c
+  if c == maxNumMsg
+    then return ()
+    else do
+      msg <- readChan mainChan
+      let username = toUser msg
+          chatBox = userChans Map.! username
+      putStrLn $ "User " ++ username ++ " received message: " ++ content msg
+      writeChan chatBox msg
+      run (c + 1) maxNumMsg mainChan userChans
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  let numUsers = read $ head args :: Int
+  let maxNumMsg = read $ args !! 1 :: Int
+
+  gen <- getStdGen
+  let userNames = take numUsers $ shuffle' availableUserName (length availableUserName) gen
+  users <- mapM (\name -> newUser name userNames) userNames
+  userChans <- mapM (\name -> newChan >>= \chan -> return (name, chan)) userNames >>= return . Map.fromList
+
+  mainChan <- newChan :: IO (Chan MessagePack)
+
+  receiverThreadIDs <- mapM (\(chan, user) -> forkIO $ receiverProcess chan (chatBoxes user)) (zip (Map.elems userChans) users)
+  userThreadIDs <- mapM (\(user, chan) -> forkIO $ userProcess user chan) (zip users (repeat mainChan))
+
+  run 0 maxNumMsg mainChan userChans
+
+-- -- select random users
+
+-- -- A global counter
+-- msgCounter <- newMVar 0
+
+-- users <- mapM (\(name, chan) -> newMVar Map.empty >>= \mvar -> return $ User name chan mvar) $ Map.toList userChans
+
+-- threadIDs <- mapM (\user -> forkIO $ userProcess user userChans msgCounter) users
+
+-- threadDelay 1000000
+-- c <- takeMVar msgCounter
+-- putStrLn $ "Total number of messages: " ++ show c
--- src/Types.hs
+++ src/Types.hs
@@ -1,31 +1,62 @@
 module Types
-  ( Message (..),
-    MessageContent,
-    Messages,
-    User (..),
+  ( MessageContent,
     UserName,
+    MessagePack (..),
+    Messages,
+    ChatBox (..),
+    ChatBoxes,
+    User (..),
+    newUser,
+    addMessages,
+    readMessages,
   )
 where
 
+import Control.Concurrent
 import qualified Data.Map as Map
 
 type MessageContent = String
 
 type UserName = String
 
-data Message = Message
+data MessagePack = MessagePack
   { content :: MessageContent,
-    user :: UserName
+    fromUser :: UserName,
+    toUser :: UserName
   }
   deriving (Show)
 
-type Messages = [Message]
+type Messages = [MessageContent]
 
-type MessageGroups = Map.Map UserName [MessageContent]
+data ChatBox = ChatBox
+  { messages :: Messages,
+    readIdx :: Int
+  }
+
+type ChatBoxes = Map.Map UserName (MVar ChatBox)
 
 data User = User
   { name :: UserName,
-    unreadMessages :: Messages,
-    readMessages :: MessageGroups
+    chatBoxes :: ChatBoxes
   }
-  deriving (Show)
+
+newUser :: UserName -> [UserName] -> IO User
+newUser name names = do
+  chatBoxes <- mapM (\name' -> newMVar ChatBox {messages = [], readIdx = 0} >>= \mvar -> return (name', mvar)) names >>= return . Map.fromList
+  return User {name = name, chatBoxes = chatBoxes}
+
+addMessages :: MVar ChatBox -> MessageContent -> IO ()
+addMessages chatBoxVar msg = do
+  chatBox <- takeMVar chatBoxVar
+  putMVar chatBoxVar chatBox {messages = msg : messages chatBox, readIdx = readIdx chatBox + 1}
+
+readMessages :: MVar ChatBox -> IO Messages
+readMessages chatBoxVar = do
+  box <- readMVar chatBoxVar
+  if readIdx box == 0
+    then return []
+    else do
+      box' <- takeMVar chatBoxVar
+      let messages' = take (readIdx box') (messages box')
+      putMVar chatBoxVar ChatBox {messages = messages box', readIdx = 0}
+      return messages'
