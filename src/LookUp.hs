fffce67b604a94c0df84fb299444c677716896a1
Chin-Yun Yu <chin-yun.yu@qmul.ac.uk>
2024-01-09 18:36:32 +0800

    add user name and message lookup table

---
+++ src/LookUp.hs
@@ -0,0 +1,42 @@
+module LookUp
+  ( availableUserName,
+    availableMessageContent,
+  )
+where
+
+availableUserName :: [String]
+availableUserName =
+  [ "Alice",
+    "Bob",
+    "Charlie",
+    "Dave",
+    "Eve",
+    "Frank",
+    "Grace",
+    "Heidi",
+    "Ivan",
+    "Judy",
+    "Mallory",
+    "Oscar",
+    "Peggy",
+    "Sybil",
+    "Trent",
+    "Walter",
+    "Wendy"
+  ]
+
+availableMessageContent :: [String]
+availableMessageContent =
+  [ "Hello",
+    "Hi",
+    "How are you?",
+    "I'm fine",
+    "Goodbye",
+    "See you",
+    "Good morning",
+    "Good afternoon",
+    "Good evening",
+    "Good night",
+    "Good day",
+    "Good luck"
+  ]
