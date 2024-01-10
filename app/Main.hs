2d9f95132bf7f87847beb5ebba69b3cad0d01ba7
Chin-Yun Yu <chin-yun.yu@qmul.ac.uk>
2024-01-10 17:07:56 +0800

    feat: send back acks

--- app/Main.hs
+++ app/Main.hs
@@ -10,32 +10,43 @@ import System.Random
 import System.Random.Shuffle
 import Types
 
-receiverProcess :: Chan MessagePack -> ChatBoxes -> IO ()
-receiverProcess incomingMessages boxes = forever $ do
-  msg <- readChan incomingMessages
-  let username = fromUser . msgDirection $ msg
-      msgContent = content msg
-      chatBox = boxes Map.! username
-  addMessage chatBox msgContent
+type TunnelPack = Either Ack MessagePack
 
-userProcess :: User -> Chan MessagePack -> IO ()
+getFromUser :: TunnelPack -> UserName
+getFromUser (Left ack) = fromUser . ackDirection $ ack
+getFromUser (Right msg) = fromUser . msgDirection $ msg
+
+getToUser :: TunnelPack -> UserName
+getToUser (Left ack) = toUser . ackDirection $ ack
+getToUser (Right msg) = toUser . msgDirection $ msg
+
+receiverProcess :: Chan TunnelPack -> ChatBoxes -> IO ()
+receiverProcess incomingMessages boxes = forever $ do
+  pack <- readChan incomingMessages
+  let username = getFromUser pack
+      chatBox = boxes Map.! username
+
+  case pack of
+    Right msg -> addMessage chatBox (content msg)
+    Left ack -> theyAcknowledge chatBox (readNum ack)
+
+userProcess :: User -> Chan TunnelPack -> IO ()
 userProcess user sendChan = forever $ do
   gen <- newStdGen
   let (randomUserIdx, gen') = randomR (0, Map.size (chatBoxes user) - 1) gen :: (Int, StdGen)
       randomUser = Map.keys (chatBoxes user) !! randomUserIdx
 
-  _ <- readMessages (chatBoxes user Map.! randomUser)
+  msgs <- readMessages (chatBoxes user Map.! randomUser)
+  writeChan sendChan (Left Ack {readNum = length msgs, ackDirection = (name user, randomUser)})
   randomMsg <- randomMessage
-  writeChan sendChan MessagePack {content = randomMsg, msgDirection = (name user, randomUser)}
-
-  -- msgs <- mapM readMessages (Map.elems $ chatBoxes user) >>= return . concat
+  writeChan sendChan (Right MessagePack {content = randomMsg, msgDirection = (name user, randomUser)})
+  addMyMessage (chatBoxes user Map.! randomUser) randomMsg
 
   -- wait for a random time
-
   let (randomDelay, _) = randomR (1, 1000000) gen' :: (Int, StdGen)
   threadDelay randomDelay
 
-run :: Int -> Int -> Chan MessagePack -> Map.Map UserName (Chan MessagePack) -> IO ()
+run :: Int -> Int -> Chan TunnelPack -> Map.Map UserName (Chan TunnelPack) -> IO ()
 run _ 0 _ _ = return ()
 run c maxNumMsg mainChan userChans = do
   hPutStr stderr $ "\r\ESC[K" ++ "Progress: " ++ show c ++ "/" ++ show maxNumMsg
@@ -44,11 +55,13 @@ run c maxNumMsg mainChan userChans = do
       hPutStrLn stderr ""
       return ()
     else do
-      msg <- readChan mainChan
-      let username = toUser . msgDirection $ msg
-          chatBox = userChans Map.! username
-      writeChan chatBox msg
-      run (c + 1) maxNumMsg mainChan userChans
+      pack <- readChan mainChan
+      let username = getToUser pack
+          chatChan = userChans Map.! username
+      writeChan chatChan pack
+      case pack of
+        Right _ -> run (c + 1) maxNumMsg mainChan userChans
+        _ -> run c maxNumMsg mainChan userChans
 
 main :: IO ()
 main = do
@@ -61,13 +74,14 @@ main = do
   users <- mapM (\name -> newUser name userNames) userNames
   userChans <- mapM (\name -> newChan >>= \chan -> return (name, chan)) userNames >>= return . Map.fromList
 
-  mainChan <- newChan :: IO (Chan MessagePack)
+  mainChan <- newChan :: IO (Chan TunnelPack)
 
   receiverThreadIDs <- mapM (\user -> forkIO $ receiverProcess (userChans Map.! name user) (chatBoxes user)) users
   userThreadIDs <- mapM (\user -> forkIO $ userProcess user mainChan) users
 
   run 0 maxNumMsg mainChan userChans
 
+  threadDelay 10000
   mapM_ killThread $ receiverThreadIDs ++ userThreadIDs
 
   -- print summary
