49b0371528a651101f9a90a101ede0e937e8bb80
Chin-Yun Yu <chin-yun.yu@qmul.ac.uk>
2024-01-17 15:41:49 +0000

    doc: comments for main

--- app/Main.hs
+++ app/Main.hs
@@ -12,40 +12,55 @@ import Types
 
 type TunnelPack = Either Ack Message
 
+-- | Get the sender from a `TunnelPack` message
 getFromUser :: TunnelPack -> UserName
 getFromUser (Left ack) = fromUser . ackDirection $ ack
 getFromUser (Right msg) = fromUser . msgDirection $ msg
 
+-- | Get the receiver from a `TunnelPack` message
 getToUser :: TunnelPack -> UserName
 getToUser (Left ack) = toUser . ackDirection $ ack
 getToUser (Right msg) = toUser . msgDirection $ msg
 
+-- | The slave process (receiver) for a user.
+-- It reads messages from the message channel and update the chat boxes
 receiverProcess :: Chan TunnelPack -> ChatBoxes -> IO ()
 receiverProcess incomingMessages boxes = forever $ do
   pack <- readChan incomingMessages
+  -- get who sent the message
   let username = getFromUser pack
       chatBox = boxes Map.! username
 
   case pack of
+    -- if it's an message, add it to the chat box
     Right msg -> addMessage chatBox (content msg)
+    -- if it's an acknowledgement, update the read number of the sender
     Left ack -> theyAcknowledge chatBox (readNum ack)
 
+-- | The user process for a user.
+-- It sends messages to a random user with a random delay (between 0 - 1 second).
+-- It also reads any unread messages from that user and acknowledge the number of messages read.
 userProcess :: User -> Chan TunnelPack -> IO ()
 userProcess user sendChan = forever $ do
   gen <- newStdGen
   let (randomUserIdx, gen') = randomR (0, Map.size (chatBoxes user) - 1) gen :: (Int, StdGen)
       randomUser = Map.keys (chatBoxes user) !! randomUserIdx
-
-  msgs <- readMessages (chatBoxes user Map.! randomUser)
-  writeChan sendChan (Left Ack {readNum = length msgs, ackDirection = (name user, randomUser)})
-  randomMsg <- randomMessage
-  writeChan sendChan (Right Message {content = randomMsg, msgDirection = (name user, randomUser)})
-  addMyMessage (chatBoxes user Map.! randomUser) randomMsg
+      (randomDelay, _) = randomR (1, 1000000) gen' :: (Int, StdGen)
 
   -- wait for a random time
-  let (randomDelay, _) = randomR (1, 1000000) gen' :: (Int, StdGen)
   threadDelay randomDelay
 
+  msgs <- readMessages (chatBoxes user Map.! randomUser)
+  -- send an acknowledgement of the number of messages read
+  writeChan sendChan (Left Ack {readNum = length msgs, ackDirection = (name user, randomUser)})
+  -- send a random message to the main process
+  randomMsg <- randomMessage
+  writeChan sendChan (Right Message {content = randomMsg, msgDirection = (name user, randomUser)})
+  -- add self message to the chat box
+  addMyMessage (chatBoxes user Map.! randomUser) randomMsg
+
+-- | The main process for running the simulation.
+-- It reads every message from users and forward them, keep track of the number of messages sent, print the progress, and stop when the number of messages sent reaches the maximum number of messages.
 run :: Int -> Int -> Chan TunnelPack -> Map.Map UserName (Chan TunnelPack) -> IO ()
 run _ 0 _ _ = return ()
 run c maxNumMsg mainChan userChans = do
@@ -58,8 +73,11 @@ run c maxNumMsg mainChan userChans = do
       pack <- readChan mainChan
       let username = getToUser pack
           chatChan = userChans Map.! username
+
+      -- forward the message to the user
       writeChan chatChan pack
       case pack of
+        -- if it's an message, increment the number of messages sent
         Right _ -> run (c + 1) maxNumMsg mainChan userChans
         _ -> run c maxNumMsg mainChan userChans
 
@@ -81,6 +99,7 @@ main = do
 
   run 0 maxNumMsg mainChan userChans
 
+  -- wait for a while to let the receiver threads process all the tracked messages
   threadDelay 10000
   mapM_ killThread $ receiverThreadIDs ++ userThreadIDs
 
